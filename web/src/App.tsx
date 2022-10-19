cb9254874ccddfc4da73f000998c156fc1449861
João Scloneski <jscloneski.dev@gmail.com>
2022-10-19 19:11:25 -0300

    wfnl

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -8,6 +8,7 @@ import logoImg from './assets/logo-nlw.svg';
 import './styles/main.css';
 
 import { CreateAdModal } from './components/CreateAdModal';
+import axios from 'axios';
 
 
 
@@ -25,10 +26,9 @@ function App() {
   const [games, setGames] = useState<Game[]>([])
 
   useEffect(() => {
-    fetch('http://localhost:3333/games')
-    .then(response => response.json())
-    .then(data => {
-      setGames(data)
+    axios('http://localhost:3333/games')
+    .then(response => {
+      setGames(response.data)
     })
   }, [])
 
